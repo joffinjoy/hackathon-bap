09ca0d19dbb954d1f2649fec3cba3311bfe9a24a
joffinjoy <joffinjoy@gmail.com>
2023-04-16 13:47:42 +0530

    Added Cold Item Scripts

--- src/scripts/mockDataGenerator/bpp/sessionTitles.js
+++ src/scripts/mockDataGenerator/bpp/sessionTitles.js
@@ -203,6 +203,8 @@ const titles = [
 	'The Science of Climate Change',
 ]
 
+console.log(titles.length)
+
 const shuffleArray = (array) => {
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1))
