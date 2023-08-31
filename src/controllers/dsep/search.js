6e7a9474f7602596b11b138117e494d64ae10a80
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2023-08-31 16:41:54 +0530

    hard coded responses for search and confirm

--- src/controllers/dsep/search.js
+++ src/controllers/dsep/search.js
@@ -8,52 +8,58 @@ const { externalRequests } = require('@helpers/requests')
 const { bppQueries } = require('@database/storage/bpp/queries')
 const { catalogService } = require('@services/catalog')
 const { searchItemListGenerator } = require('@helpers/searchItemListGenerator')
+const sampleResponses = require('../../sampleResponses/karmayogi')
 
 exports.search = async (req, res) => {
 	const failedRes = (message) => res.status(400).json({ status: false, message })
 	try {
-		const transactionId = crypto.randomUUID()
-		const messageId = crypto.randomUUID()
-		const mentorName = req.body.mentorName
-		const sessionTitle = req.body.sessionTitle
-		const type = req.body.type
-		const context = await contextBuilder(transactionId, messageId, process.env.SEARCH_ACTION)
-		let message
-		if (!mentorName && !sessionTitle) return failedRes('Either mentor Or session name must be provided')
-		if (mentorName && sessionTitle)
-			return failedRes("Hybrid searching using both mentor and session names aren't currently supported")
-		if (mentorName) message = searchMentorMessageDTO(mentorName)
-		else message = searchSessionMessageDTO(sessionTitle)
-		const searchRequestBody = requestBodyDTO(context, message)
-		await externalRequests.dsepPOST({
-			baseURL: process.env.BECKN_BG_URI,
-			body: searchRequestBody,
-			route: process.env.SEARCH_ROUTE,
+		res.status(200).json({
+			status: true,
+			message: 'Search Success',
+			data: sampleResponses.search_api_response,
 		})
-		setTimeout(async () => {
-			try {
-				let items
-				let listName
-				if (type === 'session') {
-					listName = 'sessions'
-					items = await searchItemListGenerator(transactionId, 'session')
-				} else if (type === 'mentor') {
-					listName = 'mentors'
-					items = await searchItemListGenerator(transactionId, 'mentor')
-				}
-				res.status(200).json({
-					status: true,
-					message: 'Search Success',
-					data: {
-						count: items.length,
-						[`${listName}`]: items,
-					},
-				})
-			} catch (err) {
-				console.log(err)
-				return failedRes('Something Went Wrong')
-			}
-		}, process.env.SEARCH_MINIMUM_WAIT_TIME)
+		// const transactionId = crypto.randomUUID()
+		// const messageId = crypto.randomUUID()
+		// const mentorName = req.body.mentorName
+		// const sessionTitle = req.body.sessionTitle
+		// const type = req.body.type
+		// const context = await contextBuilder(transactionId, messageId, process.env.SEARCH_ACTION)
+		// let message
+		// if (!mentorName && !sessionTitle) return failedRes('Either mentor Or session name must be provided')
+		// if (mentorName && sessionTitle)
+		// 	return failedRes("Hybrid searching using both mentor and session names aren't currently supported")
+		// if (mentorName) message = searchMentorMessageDTO(mentorName)
+		// else message = searchSessionMessageDTO(sessionTitle)
+		// const searchRequestBody = requestBodyDTO(context, message)
+		// await externalRequests.dsepPOST({
+		// 	baseURL: process.env.BECKN_BG_URI,
+		// 	body: searchRequestBody,
+		// 	route: process.env.SEARCH_ROUTE,
+		// })
+		// setTimeout(async () => {
+		// 	try {
+		// 		let items
+		// 		let listName
+		// 		if (type === 'session') {
+		// 			listName = 'sessions'
+		// 			items = await searchItemListGenerator(transactionId, 'session')
+		// 		} else if (type === 'mentor') {
+		// 			listName = 'mentors'
+		// 			items = await searchItemListGenerator(transactionId, 'mentor')
+		// 		}
+		// 		res.status(200).json({
+		// 			status: true,
+		// 			message: 'Search Success',
+		// 			data: {
+		// 				count: items.length,
+		// 				[`${listName}`]: items,
+		// 			},
+		// 		})
+		// 	} catch (err) {
+		// 		console.log(err)
+		// 		return failedRes('Something Went Wrong')
+		// 	}
+		// }, process.env.SEARCH_MINIMUM_WAIT_TIME)
 	} catch (err) {
 		console.log(err)
 	}
